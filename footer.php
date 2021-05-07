f2982e94d859c85ab23b3678b8906238bd0237f5
Vladimir-Fedorov <jon56hops@gmail.com>
2021-05-08 03:18:12 +0700

    Добавление: многостраничность при помощи php

---
+++ footer.php
@@ -0,0 +1,55 @@
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-wrapper">
+        <div class="footer-left">
+            <span class="footer-left__text text">Почта: <a href="mailto:sneglotus@ngs.ru" class="footer-left__link">sneglotus@ngs.ru</a></span>
+            <span class="footer-left__text text">Телефон: <a href="tel:+79133745429" class="footer-left__link">+7-913-374-54-29</a></span>
+            <a href="#" class="footer-left__link"><img src="img/footer_vk.png" alt="Иконка: ВКонтакте" class="footer-left__icon"></a>
+            <a href="#" class="footer-left__link"><img src="img/footer_fb.png" alt="Иконка: Фейсбук" class="footer-left__icon"></a>
+        </div>
+        <!-- /.footer-left -->
+        <div class="footer-right">
+          <span class="footer-right__text text">Instagam</span>
+          <div class="footer-galery">
+            <img src="img/inst-1.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+            <img src="img/inst-2.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+            <img src="img/inst-3.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+            <img src="img/inst-4.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+            <img src="img/inst-5.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+            <img src="img/inst-6.jpg" alt="Фотография: инстаграмм" class="footer-galery__img">
+          </div>
+          <!-- /.footer-right__galery -->
+        </div>
+        <!-- /.footer-right -->
+      </div>
+      <!-- /.footer-wrapper -->
+      <nav class="footer-menu">
+        <ul class="footer-nav">
+          <li class="footer-nav__item">
+            <a href="#" class="footer-nav__link">О клубе</a>
+          </li>
+          <li class="footer-nav__item">
+            <a href="#" class="footer-nav__link">Расписание</a>
+          </li>
+          <li class="footer-nav__item">
+            <a href="#" class="footer-nav__link">Стоимость занятий</a>
+          </li>
+          <li class="footer-nav__item">
+            <a href="#" class="footer-nav__link">Преподаватели</a>
+          </li>
+          <li class="footer-nav__item">
+            <a href="#" class="footer-nav__link">Учебный центр</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <!-- /.container -->
+  </footer>
+  <!-- /.footer -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Снежный лотос</title>
+  <link rel="stylesheet" href="css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header class="header">
+    <div class="container">
+      <div class="header-wrapper">
+        <a href="index.php" class="logo">
+          <img src="img/Логотип.png" alt="Логотип: Снежный лотос" class="logo__image">
+        </a>
+        <h1 class="header__text">11 лет мы помогаем людям найти свой путь в йоге</h1>
+        <div class="contacts">
+          <div class="contacts-phone">
+            <img src="img/Трубка.png" alt="Иконка телефона" class="contacts-phone__icon">
+            <a href="tel:+79133745429" class="contacts-phone__number">+7-913-374-54-29</a>
+          </div>
+          <!-- /.contacts-phone -->
+          <div class="social">
+            <button class="social-button">
+              <span class="social-button__burger"></span>
+            </button>
+            <div class="social-nav">
+              <a class="social-nav__link" href="#"><img class="social-nav__icon" src="img/in.svg" alt="Иконка: Инстаграм"></a>
+              <a class="social-nav__link" href="#"><img class="social-nav__icon" src="img/fb.svg" alt="Иконка: Фейсбук"></a>
+              <a class="social-nav__link" href="#"><img class="social-nav__icon" src="img/vk.svg" alt="Иконка: Вконтакте"></a>
+            </div>
+            <!-- /.social-nav -->
+          </div>
+          <!-- /.social -->
+        </div>
+        <!-- /.contacts -->
+      </div>
+      <!-- /.header-wrapper -->
+      <div class="navbar">
+        <ul class="navbar-menu">
+          <li class="navbar-menu__item">
+            <a href="#" class="navbar-menu__link">О клубе</a>
+          </li>
+          <li class="navbar-menu__item">
+            <a href="#" class="navbar-menu__link">Расписание</a>
+          </li>
+          <li class="navbar-menu__item">
+            <a href="#" class="navbar-menu__link">Стоимость занятий</a>
+          </li>
+          <li class="navbar-menu__item">
+            <a href="#" class="navbar-menu__link">Преподаватели</a>
+          </li>
+        </ul>
+        <button class="navbar__button">
+          Учебный центр
+        </button>
+      </div>
+      <!-- /.navbar -->
+    </div>
+    <!-- /.container -->
+  </header>
+  <!-- /.header --> 
