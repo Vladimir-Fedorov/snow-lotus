4582a5b791153a08af7042ceacc6b57d54f59bab
Vladimir-Fedorov <jon56hops@gmail.com>
2021-06-16 09:05:55 +0700

    Добавление: карточки и стили

--- price.php
+++ price.php
@@ -3,9 +3,9 @@
     <div class="container">
       <div class="price-wrapper">
         <div class="card">
-          <h2 class="card-header__title">Пробное занятие</h2>
+          <h3 class="card-header__title">Пробное занятие</h3>
           <span class="card-header__price">200 Р</span>
-          <div class="card-body">
+          <div class="card-body" style="height: 228px">
             <ul class="card-list">
               <li class="card-list__item">Только для новых посетителей клуба</li>
             </ul>
@@ -15,10 +15,10 @@
           <!-- /.card-body -->
         </div>
         <!-- /.card -->
-        <div class="card">
-          <h2 class="card-header__title">Разовое занятие</h2>
-          <span class="card-header__price">500 Р</span>
-          <div class="card-body">
+        <div class="card card_d">
+          <h3 class="card-header__title card-header__title_d">Разовое занятие</h3>
+          <span class="card-header__price card-header__price_d">500 Р</span>
+          <div class="card-body card-body_d">
             <ul class="card-list">
               <li class="card-list__item">Подойдет тем, у кого ненормированный рабочий график</li>
               <li class="card-list__item">Не распространяются скидки клуба</li>
@@ -30,15 +30,66 @@
         </div>
         <!-- /.card -->
         <div class="card">
-          <h2 class="card-header__title">Индивидуальное занятие</h2>
+          <h3 class="card-header__title">Индивидуальное занятие</h3>
           <span class="card-header__price">2000 Р</span>
-          <div class="card-body">
+          <div class="card-body" style="height: 228px">
             <ul class="card-list">
               <li class="card-list__item">Стоимость указана за полуторачасовое занятие</li>
               <li class="card-list__item">Не распространяются скидки клуба</li>
             </ul>
             <!-- /.card-list -->
             <button class="button card-body__button">Записаться на занятие</button>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        <div class="card">
+          <h3 class="card-header__title card-header__title_d">Абонементы<br>Хатха-йога</h3>
+          <div class="card-body">
+            <ul class="card-list">
+              <li class="card-list__item">4 занятия – 1600Р</li>
+              <li class="card-list__item">6 занятий – 2100Р</li>
+              <li class="card-list__item">8 занятий – 2400Р</li>
+              <li class="card-list__item">10 занятий – 2800Р</li>
+              <li class="card-list__item">12 занятий – 3200Р</li>
+              <li class="card-list__item">20 занятий – 5000Р</li>
+            </ul>
+            <!-- /.card-list -->
+            <button class="button card-body__button">Правила использования</button>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        <div class="card card_d">
+          <h3 class="card-header__title">Абонементы<br>Айенгар-йога</h3>
+          <div class="card-body card-body_d">
+            <ul class="card-list">
+              <li class="card-list__item">4 занятия – 1800Р</li>
+              <li class="card-list__item">6 занятий – 2400Р</li>
+              <li class="card-list__item">8 занятий – 2800Р</li>
+              <li class="card-list__item">10 занятий – 3300Р</li>
+              <li class="card-list__item">12 занятий – 3600Р</li>
+              <li class="card-list__item">20 занятий – 5600Р</li>
+            </ul>
+            <!-- /.card-list -->
+            <button class="button card-body__button">Правила использования</button>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        <div class="card">
+          <h3 class="card-header__title card-header__title_d">Абонементы<br>Смешанные направления</h3>
+          <div class="card-body">
+            <ul class="card-list">
+              <li class="card-list__item">4 занятия – 1700Р</li>
+              <li class="card-list__item">6 занятий – 2200Р</li>
+              <li class="card-list__item">8 занятий – 2600Р</li>
+              <li class="card-list__item">10 занятий – 3100Р</li>
+              <li class="card-list__item">12 занятий – 3400Р</li>
+              <li class="card-list__item">20 занятий – 5200Р</li>
+            </ul>
+            <!-- /.card-list -->
+            <button class="button card-body__button">Правила использования</button>
           </div>
           <!-- /.card-body -->
         </div>
