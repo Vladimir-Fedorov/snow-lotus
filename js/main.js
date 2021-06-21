a9483b9240fc59723a278a3ecc6f375825469e06
Vladimir-Fedorov <jon56hops@gmail.com>
2021-06-22 00:37:23 +0700

    Правки: поворот карточек

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ $('.social-button').on('click', function(e) {
   $('.social-button__burger').toggleClass('social-button__burger_active');
   $('.social-nav__link').toggleClass('social-nav__link_active');
 });
-$('.card-body__button').on('click', function(e) {
+$('.card-body__button_1').on('click', function(e) {
   e.preventDefault();
-  $('.card-front').toggleClass('card-front_active');
-  $('.card-back').toggleClass('card-back_active');
+  $('.card-front_1').toggleClass('card-front_active');
+  $('.card-back_1').toggleClass('card-back_active');
+});
+$('.card-body__button_2').on('click', function(e) {
+  e.preventDefault();
+  $('.card-front_2').toggleClass('card-front_active');
+  $('.card-back_2').toggleClass('card-back_active');
+});
+$('.card-body__button_3').on('click', function(e) {
+  e.preventDefault();
+  $('.card-front_3').toggleClass('card-front_active');
+  $('.card-back_3').toggleClass('card-back_active');
 });
 var mySwiper = new Swiper('.news-slider', {
   loop: true,
--- price.php
+++ price.php
@@ -3,20 +3,17 @@
     <div class="container">
       <div class="price-wrapper">
         <div class="card">
-          <div class="card-front">
-            <h3 class="card-header__title">Пробное занятие</h3>
-            <span class="card-header__price">200 Р</span>
-            <div class="card-body" style="height: 228px">
-              <ul class="card-list">
-                <li class="card-list__item">Только для новых посетителей клуба</li>
-                <li class="card-list__item">Можно использовать только один раз</li>
-              </ul>
-              <!-- /.card-list -->
-              <button class="button card-body__button">Записаться на занятие</button>
-            </div>
-            <!-- /.card-body -->
+          <h3 class="card-header__title">Пробное занятие</h3>
+          <span class="card-header__price">200 Р</span>
+          <div class="card-body" style="height: 228px">
+            <ul class="card-list card-list_pb45">
+              <li class="card-list__item">Только для новых посетителей клуба</li>
+              <li class="card-list__item">Можно использовать только один раз</li>
+            </ul>
+            <!-- /.card-list -->
+            <button class="button card-body__button">Записаться на занятие</button>
           </div>
-          <!-- /.card-front -->
+          <!-- /.card-body -->
         </div>
         <!-- /.card -->
         <div class="card card_d">
@@ -37,7 +34,7 @@
           <h3 class="card-header__title">Индивидуальное занятие</h3>
           <span class="card-header__price">2000 Р</span>
           <div class="card-body" style="height: 228px">
-            <ul class="card-list">
+            <ul class="card-list card-list_pb45">
               <li class="card-list__item">Стоимость указана за полуторачасовое занятие</li>
               <li class="card-list__item">Не распространяются скидки клуба</li>
             </ul>
@@ -48,10 +45,10 @@
         </div>
         <!-- /.card -->
         <div class="card">
-          <div class="card-front">
+          <div class="card-front card-front_1">
             <h3 class="card-header__title card-header__title_d">Абонементы<br>Хатха-йога</h3>
             <div class="card-body">
-              <ul class="card-list">
+              <ul class="card-list card-list_pb45">
                 <li class="card-list__item">4 занятия – 1600Р</li>
                 <li class="card-list__item">6 занятий – 2100Р</li>
                 <li class="card-list__item">8 занятий – 2400Р</li>
@@ -60,12 +57,12 @@
                 <li class="card-list__item">20 занятий – 5000Р</li>
               </ul>
               <!-- /.card-list -->
-              <button class="button card-body__button">Правила использования</button>
+              <button class="button card-body__button card-body__button_1">Правила использования</button>
             </div>
             <!-- /.card-body -->
           </div>
           <!-- /.card-front -->
-          <div class="card-back">
+          <div class="card-back card-back_1">
             <h3 class="card-header__title card-header__title_d">Правила<br>Хатха-йога</h3>
             <div class="card-body">
               <ul class="card-list">
@@ -76,7 +73,7 @@
                 <li class="card-list__item">на все абонементы, кроме абонемента на 20 занятий, действуют «семейная» и «пенсионная» скидки 5%</li>
               </ul>
               <!-- /.card-list -->
-              <button class="button card-body__button">Стоимость абонемента</button>
+              <button class="button card-body__button card-body__button_1">Стоимость абонемента</button>
             </div>
             <!-- /.card-body -->
           </div>
@@ -84,37 +81,75 @@
         </div>
         <!-- /.card -->
         <div class="card card_d">
-          <h3 class="card-header__title">Абонементы<br>Айенгар-йога</h3>
-          <div class="card-body card-body_d">
-            <ul class="card-list">
-              <li class="card-list__item">4 занятия – 1800Р</li>
-              <li class="card-list__item">6 занятий – 2400Р</li>
-              <li class="card-list__item">8 занятий – 2800Р</li>
-              <li class="card-list__item">10 занятий – 3300Р</li>
-              <li class="card-list__item">12 занятий – 3600Р</li>
-              <li class="card-list__item">20 занятий – 5600Р</li>
-            </ul>
-            <!-- /.card-list -->
-            <button class="button card-body__button">Правила использования</button>
+          <div class="card-front card-front_2">
+            <h3 class="card-header__title">Абонементы<br>Айенгар-йога</h3>
+            <div class="card-body card-body_d">
+              <ul class="card-list card-list_pb45">
+                <li class="card-list__item">4 занятия – 1800Р</li>
+                <li class="card-list__item">6 занятий – 2400Р</li>
+                <li class="card-list__item">8 занятий – 2800Р</li>
+                <li class="card-list__item">10 занятий – 3300Р</li>
+                <li class="card-list__item">12 занятий – 3600Р</li>
+                <li class="card-list__item">20 занятий – 5600Р</li>
+              </ul>
+              <!-- /.card-list -->
+              <button class="button card-body__button card-body__button_2">Правила использования</button>
+            </div>
+            <!-- /.card-body -->
           </div>
-          <!-- /.card-body -->
+          <!-- /.card-front -->
+          <div class="card-back card-back_2">
+            <h3 class="card-header__title card-header__title">Правила<br>Айенгар-йога</h3>
+            <div class="card-body card-body_d">
+              <ul class="card-list">
+                <li class="card-list__item">по данным абонементам вы можете посещать занятия любых преподавателей по направлению айенгар-йога</li>
+                <li class="card-list__item">по абонементу может ходить только один человек, исключением является абонемент на 20 занятий</li>
+                <li class="card-list__item">все абонементы действуют 30 дней со дня активации</li>
+                <li class="card-list__item">все абонементы необходимо активировать в течение 3 месяцев со дня покупки</li>
+                <li class="card-list__item">на все абонементы, кроме абонемента на 20 занятий, действуют «семейная» и «пенсионная» скидки 5%</li>
+              </ul>
+              <!-- /.card-list -->
+              <button class="button card-body__button card-body__button_2">Стоимость абонемента</button>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card-back -->
         </div>
         <!-- /.card -->
         <div class="card">
-          <h3 class="card-header__title card-header__title_d">Абонементы<br>Смешанные направления</h3>
-          <div class="card-body">
-            <ul class="card-list">
-              <li class="card-list__item">4 занятия – 1700Р</li>
-              <li class="card-list__item">6 занятий – 2200Р</li>
-              <li class="card-list__item">8 занятий – 2600Р</li>
-              <li class="card-list__item">10 занятий – 3100Р</li>
-              <li class="card-list__item">12 занятий – 3400Р</li>
-              <li class="card-list__item">20 занятий – 5200Р</li>
-            </ul>
-            <!-- /.card-list -->
-            <button class="button card-body__button">Правила использования</button>
+          <div class="card-front card-front_3">
+            <h3 class="card-header__title card-header__title_d">Абонементы<br>Смешанные направления</h3>
+            <div class="card-body">
+              <ul class="card-list card-list_pb45">
+                <li class="card-list__item">4 занятия – 1700Р</li>
+                <li class="card-list__item">6 занятий – 2200Р</li>
+                <li class="card-list__item">8 занятий – 2600Р</li>
+                <li class="card-list__item">10 занятий – 3100Р</li>
+                <li class="card-list__item">12 занятий – 3400Р</li>
+                <li class="card-list__item">20 занятий – 5200Р</li>
+              </ul>
+              <!-- /.card-list -->
+              <button class="button card-body__button card-body__button_3">Правила использования</button>
+            </div>
+            <!-- /.card-body -->
           </div>
-          <!-- /.card-body -->
+          <!-- /.card-front -->
+          <div class="card-back card-back_3">
+            <h3 class="card-header__title card-header__title_d">Правила<br>Смешанные направления</h3>
+            <div class="card-body">
+              <ul class="card-list">
+                <li class="card-list__item">по данным абонементам вы можете посещать занятия по разным направлениям в равном количестве</li>
+                <li class="card-list__item">по абонементу может ходить только один человек, исключением является абонемент на 20 занятий</li>
+                <li class="card-list__item">все абонементы действуют 30 дней со дня активации</li>
+                <li class="card-list__item">все абонементы необходимо активировать в течение 3 месяцев со дня покупки</li>
+                <li class="card-list__item">на все абонементы, кроме абонемента на 20 занятий, действуют «семейная» и «пенсионная» скидки 5%</li>
+              </ul>
+              <!-- /.card-list -->
+              <button class="button card-body__button card-body__button_3">Стоимость абонемента</button>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card-back -->
         </div>
         <!-- /.card -->
       </div>
